ae0b90650855206ecfae085ba8016104262281f2
Floatingdream20 <2161979329@qq.com>
2021-01-07 20:44:15 +0800

    finished

--- main.go
+++ main.go
@@ -3,15 +3,15 @@ package main
 import (
 	"Bitcoin_core_web/dbMysqlSV"
 	_ "Bitcoin_core_web/routers"
+	"fmt"
 	"github.com/astaxie/beego"
 )
 
 func main() {
 	dbMysqlSV.ConDB()
-
-beego.SetStaticPath("/js","./static/js")
-beego.SetStaticPath("/css","./static/css")
-beego.SetStaticPath("/images","./static/images")
+	fmt.Println("hello world")
+	beego.SetStaticPath("/js", "./static/js")
+	beego.SetStaticPath("/css", "./static/css")
+	beego.SetStaticPath("/images", "./static/images")
 	beego.Run()
 }
-
